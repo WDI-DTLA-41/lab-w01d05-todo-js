6515b451deb510ed5700b69ee01858cd4101eb53
Abi Munoz <abimunozr@gmail.com>
2016-12-04 22:45:51 -0800

    adds css styles

---
+++ js-scratchpad.js
@@ -0,0 +1,44 @@
+
+// ////////////////////////////////////////
+// // function todoList () {
+// //   var item= document.getElementById("new-todo").value
+// //   var text= document.createTextNode("item")
+// //   var newItem = document.createElement("li")
+// //   newItem.appendChild(text)
+// //   document.getElementById("todoList").appendChild(newItem)
+// // }
+
+// ////////////////////////////////////////
+// //input
+// var input = document.querySelector('input');
+// var handleKeypress = function(event) {
+//   console.log(event.target, event.currentTarget, event.keyCode);
+// }
+// input.addEventListener('keypress', handleKeypress);
+
+
+// // // render input as list
+// // // works but only adds single element to list
+// var inInput = document.querySelector('input');
+// var inReturn = document.querySelector('ul');
+// var newItem = document.createElement("li")
+
+// var handleEnter = function(event) {
+//   if(event.keyCode === 13) {
+//     // inReturn.textContent = event.target.value;
+//     // event.target.classList.add('li');
+//     newItem.appendChild(inReturn)
+//     document.getElementById("#todo").appendChild(newItem)
+//   }
+// }
+
+// inInput.addEventListener('keyup', handleEnter)
+
+// ////////////////////////////////////////
+
+// // another attempt at adding new elements to list
+// // var list = document.document.getElementsByClassName('todo');
+// // var input = document.document.getElementsByClassName('new-todo').value;
+// // var entry = document.createElement('li');
+// // entry.appendChild(document.createTextNode(input));
+// // list.appendChild(entry);
