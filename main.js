86739f991a6267c3b72f5059f06018aa34034b76
Jake Miller <hillsighed@gmail.com>
2016-12-04 18:25:53 -0800

    styling and feature to alert user to enter text if trtying ti add a null value

--- main.js
+++ main.js
@@ -3,48 +3,50 @@ console.log("Linked")
 // Function to determine when user presses enter
 var addTask = function (event){
   if (event.keyCode === 13){
-    //console.log(event);
-    console.log(event.target.value);
-    console.log("User Pressed Enter");
-    console.log('<li>' + event.target.value + '</li>');
+    if (event.target.value !== ""){
+      console.log(event);
+      console.log(event.target.value);
+      console.log("User Pressed Enter");
+      console.log('<li>' + event.target.value + '</li>');
+      // Create new List Item
+      var newLi = document.createElement("li")
 
-    // Create new List Item
-    var newLi = document.createElement("li")
+      // Add class to new List Item
+      newLi.className='todo'
 
-    // Add class to new List Item
-    newLi.className='todo'
+      // Add span and User's text
+      newLi.innerHTML = '<span class="remove">x</span>' + ' ' + event.target.value;
 
-    // Add span and User's text
-    newLi.innerHTML = '<span class="remove">x</span>' + ' ' + event.target.value;
+      // Clear our input box
+      event.target.value = '';
 
-    // Clear our input box
-    event.target.value = '';
+      // Get the ToDo List unOrdered List
+      var todoList = document.querySelector('ul');
 
-    // Get the ToDo List unOrdered List
-    var todoList = document.querySelector('ul');
+      // Append our new li to the todo List
+      todoList.appendChild(newLi);
 
-    // Append our new li to the todo List
-    todoList.appendChild(newLi);
+      // Add Event Listener to remove Item // when text is clicked remove item
+      // newLi.addEventListener('click', deleteTask);
 
-    // Add Event Listener to remove Item // when text is clicked remove item
-    // newLi.addEventListener('click', deleteTask);
+      // Collect all span items in the todo list
+      var todoItems = document.querySelectorAll('.remove');
 
-    // Collect all span items in the todo list
-    var todoItems = document.querySelectorAll('.remove');
+      // iterate through array of todo items and add an event
+      for (var i = 0; i < todoItems.length; i++){
+        todoItems[i].addEventListener('click',deleteTask);
+      };
 
-    // iterate through array of todo items and add an event
-    for (var i = 0; i < todoItems.length; i++){
-      todoItems[i].addEventListener('click',deleteTask);
-    };
-
-    var tasks = document.querySelectorAll('.todo');
-    for (var i = 0; i < tasks.length; i++){
-      tasks[i].addEventListener('click',markTaskCompletionState);
-      tasks[i].addEventListener('dblclick',markTaskImportant);
-    };
+      var tasks = document.querySelectorAll('.todo');
+      for (var i = 0; i < tasks.length; i++){
+        tasks[i].addEventListener('click',markTaskCompletionState);
+        tasks[i].addEventListener('dblclick',markTaskImportant);
+      };
     /// EXPERIMENTAL // for important tasks
 
     /// END Experimental ///
+    }
+    else{alert("Enter Todo")}
   };
 };
 
