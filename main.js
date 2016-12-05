01dc4d6c06c919798fba9d627ea792976e91c20e
kora colasuonno <kora.colasuonno@gmail.com>
2016-12-04 18:14:29 -0800

    creates new li every time event listener is fired

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
+
+
+
 var input = document.querySelector('.new-todo');
-var li = document.createElement('li');
-var newLI = document.body.querySelector('ul').appendChild(li);
 
 var addItem = function(event) {
   console.log(event.target, event.target.value, event.keyCode);
+  var li = document.createElement('li');
+  var newLI = document.body.querySelector('ul').appendChild(li);
   if (event.keyCode === 13){
     newLI.textContent = event.target.value;
     event.target.value = '';
   }
 }
+
+
 input.addEventListener('keyup', addItem);
