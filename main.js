70fabdeb509d3ff9109c91b91ba95f4bef6b549b
Abi Munoz <abimunozr@gmail.com>
2016-12-05 15:49:08 -0800

    adds new elements to list

--- main.js
+++ main.js
@@ -1,25 +1,63 @@
 console.log ("linked!");
 
-//input
-var input = document.querySelector('input');
-var handleKeypress = function(event) {
-  console.log(event.target, event.currentTarget, event.keyCode);
-}
-input.addEventListener('keypress', handleKeypress);
-
-
-// render input as <li>, only adds single element to list
-var inInput = document.querySelector('input');
-var inReturn = document.querySelector('li');
+// create new list
+var input = document.querySelector('.new-todo');               // selects <input class= "new-todo">
+var inReturn = document.querySelector('.todo');                // selects <ul class= "todo">
 
 var handleEnter = function(event) {
   if(event.keyCode === 13) {
-    inReturn.textContent = event.target.value;
-    event.target.classList.add('li');
+    // create element
+    var newElement = document.createElement("li");
+    // add text to element
+    newElement.textContent = event.target.value;
+    // append to parent
+    inReturn.appendChild(newElement);
+    event.currentTarget.value = " ";
   }
 }
 
-inInput.addEventListener('keyup', handleEnter)
+input.addEventListener('keyup', handleEnter)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//input
+// var input = document.querySelector('input');
+// var handleKeypress = function(event) {
+//   console.log(event.target, event.currentTarget, event.keyCode);
+// }
+// input.addEventListener('keypress', handleKeypress);
+
+
+/// WORKS/////////////////////////////////////////////////
+// render input as <li>, only adds single element to list
+// var input = document.querySelector('input');
+// var inReturn = document.querySelector('li');
+
+// var handleEnter = function(event) {
+//   if(event.keyCode === 13) {
+//     inReturn.textContent = event.target.value;
+//     event.target.classList.add('li');
+//   }
+// }
+
+// input.addEventListener('keyup', handleEnter)
+/// WORKS/////////////////////////////////////////////////
 
 
 
