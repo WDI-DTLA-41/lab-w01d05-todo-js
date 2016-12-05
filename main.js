ea04b0d513d3358e61684621f400904c322628da
kora colasuonno <kora.colasuonno@gmail.com>
2016-12-05 14:44:46 -0800

    adds all required basic functionality

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
-
-
+// adds input field's text content as new list item
 
 var input = document.querySelector('.new-todo');
 
 var addItem = function(event) {
-  console.log(event.target, event.target.value, event.keyCode);
-  var li = document.createElement('li');
-  var newLI = document.body.querySelector('ul').appendChild(li);
   if (event.keyCode === 13){
-    newLI.textContent = event.target.value;
+    var li = document.createElement('li');
+    li.classList.add('todo');
+    var newLI = document.body.querySelector('ul').appendChild(li);
+    newLI.innerHTML = '<span class="remove">x</span>' + ' ~ ' + event.target.value;
     event.target.value = '';
-  }
-}
-
-
+  };
+};
 input.addEventListener('keyup', addItem);
+
+
+// remove list item when span 'x' is clicked
+var ul = document.querySelector('.todos');
+var x = document.querySelector('.remove');
+
+ul.addEventListener('click', function(event) {
+  if (event.target.tagName.toLowerCase() === 'span') {
+    event.target.parentNode.remove();
+  }
+});
+
+ul.addEventListener('click', function(event) {
+  if (event.target.tagName.toLowerCase() === 'li') {
+    event.target.classList.toggle('complete');
+  }
+});
+
+
+////////// .classList.contains
