249bd4e0184bd6f665226f3cecaed50c8c39ce00
Lacey Madison <laceyannmadison@gmail.com>
2016-12-06 10:46:08 -0800

    everything works

--- main.js
+++ main.js
@@ -4,36 +4,46 @@
 // - [x] the 'li' should have a class of "todo"
 // - [x] the 'li' should contain a 'span' with a class of 'remove' and the text content 'x'
 // - [x] Appdn the 'li' to 'ul.todos'
+// ### When a user clicks on a todo's span's "x"
+// - [x] remove the todo from the DOM
+
+
 
 var input = document.querySelector('.new-todo');
-
+var listItems = document.querySelector('ul');
+var todoList = document.querySelector('.todos');
 var createTodo = function(event) {
   var newTodo = event.target.value;
-  var todoList = document.querySelector('.todos');
   var newListItem = document.createElement('li');
+  var removeItem = function() {
+  this.parentNode.remove();
 
+}
   if (event.keyCode === 13) {
     newListItem.className = 'todo';
+    var todoList = document.querySelector('.todos');
     todoList.appendChild(newListItem);
     newListItem.textContent=newTodo;
-    var children = todoList.children.length + 1;
-    newListItem.setAttribute("id", "element"+children);
+    var children = todoList.children.length;
+    newListItem.setAttribute("id", children);
     todoList.appendChild(newListItem);
     event.target.value = '';
+    newListItem.innerHTML='<span class="remove"> x </span>' + newTodo;
+    var clickToRemove = document.querySelectorAll('.remove');
+    for (var i = 0; i < clickToRemove.length; i++){
+      clickToRemove[i].addEventListener('click', removeItem);
+
+    }
   }
-  // newListItem.innerHTML=newTodo + '<span class="remove"> x</span>';
+
 }
 
 
-// ### When a user clicks on a todo's span's "x"
-// - [ ] remove the todo from the DOM
-
-
-
+// attach eventListener after span is created or put it on a parent node (use classList.contains)
 
 // ### When a user click's on the todo text
-// - [ ] add or remove the class "complete"
-// - [ ] "complete" should add a line through the text
+// - [x] add or remove the class "complete"
+// - [x] "complete" should add a line through the text
 
 var clickOn = function() {
   var listItem = event.target;
@@ -58,6 +68,5 @@ var checkList = function() {
   }
 }
 
-var listClick = document.querySelector('ul');
 input.addEventListener('keyup', createTodo);
-listClick.addEventListener('click', checkList);
+listItems.addEventListener('click', checkList);
